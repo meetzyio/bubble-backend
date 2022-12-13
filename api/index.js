501766ce8193d1e78d4bbcbd2f027e6fe005668b
meetzyio <95203277+meetzyio@users.noreply.github.com>
2022-12-13 06:56:47 +0000

    fdsfs

--- api/index.js
+++ api/index.js
@@ -2,7 +2,6 @@ import 'dotenv/config';
 import cors from 'cors';
 import express from 'express';
 const { v4 } = require('uuid');
-
 import bubble from './endpoints/bubble';
 import hubspot from './endpoints/hubspot';
 
