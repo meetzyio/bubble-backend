68feb1c61cfac4707fa6cb101e6b9c8ca1785481
meetzyio <95203277+meetzyio@users.noreply.github.com>
2022-12-13 12:00:31 +0000

    rtr

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`;
+  const path = `/api/fields`;
   res.setHeader('Content-Type', 'text/html');
   res.setHeader(
     'Cache-Control',
